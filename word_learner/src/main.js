c39e804e49a79101916d78477ddfae93893d9418
DocCrd <>
2021-11-23 15:06:50 +0300

    finally upload

--- word_learner/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './vuex/store'
-
-store.commit('initStudyMassive')
-store.commit('updateWord')
-createApp(App).use(store).use(router).mount('#app')
--- word_learner/src/vuex/getters/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  current: state => state.current,
-  wholeWords: state => state.wholeWords,
-  aboutCash: state => state.aboutCash,
-  checks: state => state.checks
-}
